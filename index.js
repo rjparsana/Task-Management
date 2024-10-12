e7fb1905ce179e125f4b03e7f63aaddb2baf97f3
rjparsana <160236867+rjparsana@users.noreply.github.com>
2024-10-13 02:08:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/task');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Task Management API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
